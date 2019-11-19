825cabfac792348742290d880932bc9b7c864dba
Nick Christman <nick.p.christman@gmail.com>
2019-11-19 01:42:53 -0800

    updated netcdf2csv

--- scratch/netcdf2csv.py
+++ scratch/netcdf2csv.py
@@ -6,13 +6,14 @@ import os
 import matplotlib.pyplot as plt
 # from mpl_toolkits.basemap import Basemap
 
-nc_dir = '/home/christnp/Downloads/INM-CM4_rcp45_2025_Monthly_netCDF/'
-nc_file = 'INM-CM4_rcp45_2025_Tmax01.nc'
+nc_dir = '/home/christnp/Development/e6893/homework/e6893-project/scratch/data/ftp.star.nesdis.noaa.gov-static/'
+nc_file = 'VHP.G04.C07.NN.P2006001.VH.nc'
 nc_path = os.path.join(nc_dir,nc_file)
 fh = Dataset(nc_path, mode='r')
-easting = fh.variables['easting'][:]
-northing = fh.variables['northing'][:]
-tmax = fh.variables['Tmax01'][:]
+# easting = fh.variables['easting'][:]
+# northing = fh.variables['northing'][:]
+# tmax = fh.variables['Tmax01'][:]
 
-lon_0 = easting.mean()
-lat_0 = northing.mean()
+print(fh.variables['VHI'][3615][9999])
+# lon_0 = easting.mean()
+# lat_0 = northing.mean()
